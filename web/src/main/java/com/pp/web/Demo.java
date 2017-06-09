d0c45fc44c9b8508b42c4f5d36a682c7594c976d
chenlijun <419166044@qq.com>
2017-06-09 21:44:48 +0800

    测试

---
+++ web/src/main/java/com/pp/web/Demo.java
@@ -0,0 +1,12 @@
+package com.pp.web;
+
+import com.pp.common.CommonUtils;
+
+/**
+ * Created by chenlijun on 2017/6/3.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(CommonUtils.mmmm);
+    }
+}
